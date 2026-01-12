4e415d25f63f17c151ed6ade6cf7c4c72a7475cb
Rahul Chemitiganti <81369865+rahulansharma682@users.noreply.github.com>
2026-01-12 22:52:57 +0530

    Time: 2327 ms (22.54%) | Memory: 12.9 MB (39.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,12 +5,32 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        key = ''.join(sorted(s1))
-        for i in range(len(s2) - len(key) + 1):
-            r = s2[i:i+len(key)]
-            if key == ''.join(sorted(r)):
+        hM_s1 = {}
+        s1Len = len(s1)
+
+        for c in s1:
+            if c in hM_s1:
+                hM_s1[c] += 1
+            else:
+                hM_s1[c] = 1
+        
+        for i in range(len(s2) - s1Len + 1):
+            comp = s2[i:i+s1Len]
+            hM_s2 = {}
+
+            for c in comp:
+                if c in hM_s2:
+                    hM_s2[c] += 1
+                else:
+                    hM_s2[c] = 1
+                    
+            if hM_s1 == hM_s2:
                 return True
+        
         return False
+
+        
+        
 
         
         
