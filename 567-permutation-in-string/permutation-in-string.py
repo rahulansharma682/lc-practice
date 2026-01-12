8984ce07c7d95e475c2a336d7a683e2e6a8ab0ab
Rahul Chemitiganti <81369865+rahulansharma682@users.noreply.github.com>
2026-01-12 23:01:51 +0530

    Time: 15 ms (98.40%) | Memory: 12.9 MB (39.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,7 +6,12 @@ class Solution(object):
         :rtype: bool
         """
         hM_s1 = {}
+        hM_s2 = {}
         s1Len = len(s1)
+        s2Len = len(s2)
+
+        if s1Len > s2Len:
+            return False
 
         for c in s1:
             if c in hM_s1:
@@ -14,23 +19,31 @@ class Solution(object):
             else:
                 hM_s1[c] = 1
         
-        for i in range(len(s2) - s1Len + 1):
-            comp = s2[i:i+s1Len]
-            hM_s2 = {}
+        for i in range(s1Len):
+            c = s2[i]
+            if c in hM_s2:
+                hM_s2[c] += 1
+            else:
+                hM_s2[c] = 1
+        
+        if hM_s1 == hM_s2:
+            return True
+        
+        for r in range(s1Len, s2Len):
+            newChar = s2[r]
 
-            for c in comp:
-                if c in hM_s2:
-                    hM_s2[c] += 1
-                else:
-                    hM_s2[c] = 1
-                    
+            if newChar in hM_s2:
+                hM_s2[newChar] += 1
+            else:
+                hM_s2[newChar] = 1
+            
+            removeOldChar = s2[r - s1Len]
+            hM_s2[removeOldChar] -= 1
+
+            if hM_s2[removeOldChar] == 0:
+                del hM_s2[removeOldChar]
+            
             if hM_s1 == hM_s2:
                 return True
-        
+
         return False
-
-        
-        
-
-        
-        
